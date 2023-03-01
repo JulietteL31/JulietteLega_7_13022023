adcac8c5fbcfba6f155c912cc6d9513ac32cda85
Juliette Lega <legajuliette@gmail.com>
2023-03-01 23:25:33 +0100

    ajout commentaire explicatif et suppr accolades

--- src/pages/Logement/Logement.js
+++ src/pages/Logement/Logement.js
@@ -1,4 +1,3 @@
-// import image from "../../assets/interieur_Kasa.png"
 import DropdownHost from '../../components/Dropdown/Dropdown'
 import Carousel from '../../components/Carousel/Carousel'
 import './Logement.css'
@@ -7,11 +6,12 @@ import Stars from "../../components/Stars/Stars"
 import listeLogements from '../../data/logement_data.json'
 import { useParams } from "react-router-dom"
 import {  useState, useEffect } from "react"
-import  Error from '../Error/Error'
+import Error from '../Error/Error'
 
 
 function Logement() {
 
+//Mise en place du useState pour récupérer les infos du logement
     const [logement, setLogement] = useState({
         tags: [],
         equipments:[],
@@ -23,9 +23,11 @@ function Logement() {
             picture: ""
         }
     });
-    
+
+//Récup id logement cliqué avec useParams
     const { id } = useParams()
 
+//Récup et affichage des infos relatives au logement dans la liste de datas en fonction de l'id avec useEffect
     useEffect(() => {
             listeLogements.map((house) => {
             if(house.id === id) {
@@ -34,16 +36,13 @@ function Logement() {
         })
     }, [id]);
   
+//Gestion des erreurs: si le logement n'existe pas => page 404
+    if(logement.title === undefined){
+        return <Error /> }
 
-    if(logement.title === undefined){{
-       return <Error />
-    }}
- 
 
-    console.log(logement)
-
+//Affichage description et équipements dans balises adaptées
     const logementEquipement = logement.equipments;
-
 
     let mapLogementEquipement = logementEquipement.map((l, index) => (
         <p className="dropdown-ul" key={l}>{logement.equipments[index]}</p>
@@ -51,6 +50,7 @@ function Logement() {
 
     let affichageDescription = <p className='text'> {logement.description} </p>
 
+//Affichage infos page logement
     return (
         <div>
         <section className='fiche-logement'>
@@ -79,56 +79,6 @@ function Logement() {
             </div>
 
     )
-
-    
-    /*
-    const { id } = useParams()
-    const logements = listeLogements
-    console.log(id)
-
-    const element = logements.find(el => el.id === id)
-    console.log(element.title)
-    const equipements = element.equipments
-
-    const equipementsMapper = <ul className='dropdown-ul'> {equipements.map((equipement) =>
-    <li key={equipement}>{equipement}</li>
-    )}</ul>;
-    
-console.log(equipements)
-const description = <p className='text'>{element.description}</p>
-
-    if(logements.find(el => el.id === id)) {
-        return(
-            <div>
-                <section className='fiche-logement'>
-                    <Carousel cover={element.cover} pictures={element.pictures}/>
-                    <div className='description-container'>
-                        <div className='title-and-tags'>
-                            <div className='title'>
-                                <h1 className='title-name'>{element.title}</h1>
-                                <h2 className='title-direction'>{element.location}</h2>
-                            </div>
-                            <Tags tags={element.tags}/>
-                        </div>
-                        <div className='name-and-note'> 
-                            <div className='host'>
-                                <div className='host-name'><p>{element.host.name}</p></div>
-                                <div className='host-picture'><img src={element.host.picture} alt='host' className="img-host"/></div>
-                            </div>
-                            <Stars rate={element.rating}/>
-                        </div>
-                    </div>
-                    <div className='dropdown-container'>
-                        <DropdownHost title="Description" description={description} classeTitre="dropdown-titre" classeText="dropdown-text" classe="dropdown" fontTitre="title-drop"/>
-                        <DropdownHost title="Equipements" description={equipementsMapper} classeTitre="dropdown-titre" classeText="dropdown-text" classe="dropdown" fontTitre="title-drop"/>
-                    </div>
-                </section>
-            </div>
-        )
-    } else {
-        return (redirect('/404'))
-    }
-    */
 }
 
 export default Logement
