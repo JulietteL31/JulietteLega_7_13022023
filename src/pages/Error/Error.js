a66caf4b2a42f295f56974f672fbb550f8a16acb
Juliette Lega <legajuliette@gmail.com>
2023-03-06 19:36:14 +0100

    ajout header dans page 404

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,10 +1,12 @@
 import error from '../../assets/404.png'
 import {Link} from 'react-router-dom'
 import './Error.css'
+import Header from '../../components/Header/Header'
 
 function Error() {
     return(
         <div>
+            <Header linkAbout="noUnderline" linkHome="noUnderline"/>
             <section className='error404-container'>
                 <img src={error} className='error404' alt='404'/>
                 <h1 className='error404-text'>Oups ! La page que vous demandez n'existe pas.</h1>
